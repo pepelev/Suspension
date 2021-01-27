194dc8ba2da95d550feaa4a3d04a6cdb943a7c43
pepelev <kran440@gmail.com>
2021-01-27 23:33:06 +0500

    Coroutines2 now returns entry and exit too, but exit is completely wrong

--- src/Suspension.SourceGenerator/Coroutines2.cs
+++ src/Suspension.SourceGenerator/Coroutines2.cs
@@ -52,97 +52,27 @@ namespace Suspension.SourceGenerator
                 .GetEnumerator();
         }
 
-        private IEnumerable<(string From, string To)> Graph(MethodDeclarationSyntax method, SemanticModel semantic)
-        {
-            var graph = ControlFlowGraph.Create(method, semantic);
-            var entry = graph.Blocks.Single(block => block.Kind == BasicBlockKind.Entry);
-            var exit = graph.Blocks.Single(block => block.Kind == BasicBlockKind.Exit);
-
-            var suspensionPoints = graph.Blocks.Select(
-                    block => new
-                    {
-                        Block = block,
-                        SuspensionPoints = block.Operations
-                            .That(new SuspensionPoint.Is())
-                            .Select(new SuspensionPoint.Name())
-                            .ToList()
-                    }
-                )
-                .Prepend(new {Block = entry, SuspensionPoints = new List<string> {"Entry"}})
-                .Append(new {Block = exit, SuspensionPoints = new List<string> {"Exit"}})
-                .Where(pair => pair.SuspensionPoints.Any())
-                .ToDictionary(pair => pair.Block, pair => pair.SuspensionPoints);
-
-            foreach (var names in suspensionPoints.Values)
-            {
-                foreach (var pair in names.Pairwise())
-                {
-                    yield return pair;
-                }
-            }
-
-            foreach (var pair in suspensionPoints)
-            {
-                var startBlock = pair.Key;
-                var points = pair.Value;
-
-                var visited = new HashSet<BasicBlock> {startBlock};
-                var queue = new Queue<BasicBlock>();
-                {
-                    if (startBlock.ConditionalSuccessor is { } conditional)
-                    {
-                        queue.Enqueue(conditional.Destination);
-                    }
-
-                    if (startBlock.FallThroughSuccessor is { } fallThrough)
-                    {
-                        queue.Enqueue(fallThrough.Destination);
-                    }
-                }
-
-                while (queue.Count > 0)
-                {
-                    var block = queue.Dequeue();
-
-                    if (suspensionPoints.TryGetValue(block, out var list))
-                    {
-                        yield return (points.Last(), list[0]);
-                    }
-                    else
-                    {
-                        if (visited.Contains(block))
-                            continue;
-
-                        if (block.ConditionalSuccessor is { } conditional)
-                        {
-                            queue.Enqueue(conditional.Destination);
-                        }
-
-                        if (block.FallThroughSuccessor is { } fallThrough)
-                        {
-                            queue.Enqueue(fallThrough.Destination);
-                        }
-                    }
-
-                    visited.Add(block);
-                }
-            }
-        }
-
         private IEnumerable<SyntaxTree> MakeSuspendable(MethodDeclarationSyntax method, SemanticModel semantic)
         {
             var symbol = semantic.GetDeclaredSymbol(method) ?? throw new Exception("GetDeclaredSymbol failed");
             var graph = ControlFlowGraph.Create(method, semantic);
-            var entry = graph.Blocks.Single(block => block.Kind == BasicBlockKind.Entry);
+            var entry = graph.Entry();
 
             var references = new Graph3(graph).ToDictionary(pair => pair.Suspension, pair => pair.References);
+
+            SyntaxTree Tree(string name, FlowPoint point)
+            {
+                var coroutine = new Dumb(name, symbol, point, references[name]);
+                return coroutine.Document;
+            }
+
+            yield return Tree("Entry", new FlowPoint(entry, 0));
 
             // todo put this code into bfs class
             var visited = new HashSet<BasicBlock>();
             var queue = new Queue<BasicBlock>();
             queue.Enqueue(entry);
 
-            var result = new List<Dumb>();
             while (queue.Count > 0)
             {
                 var block = queue.Dequeue();
@@ -155,7 +85,7 @@ namespace Suspension.SourceGenerator
                     if (operation.Accept(new SuspensionPoint.Is()))
                     {
                         var name = operation.Accept(new SuspensionPoint.Name());
-                        result.Add(new Dumb(name, symbol, new FlowPoint(block, i + 1), references[name]));
+                        yield return Tree(name, new FlowPoint(block, i + 1));
                     }
                 }
 
@@ -171,10 +101,7 @@ namespace Suspension.SourceGenerator
                 }
             }
 
-            foreach (var dumb in result)
-            {
-                yield return dumb.Document;
-            }
+            yield return Tree("Exit", new FlowPoint(graph.Exit(), 0));
         }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
