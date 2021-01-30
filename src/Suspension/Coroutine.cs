50028924f768a352bd5fd7cb7e82f840b7f0b556
pepelev <kran440@gmail.com>
2021-01-30 22:06:46 +0500

    removes unused Coroutine.Continuation

--- src/Suspension/Coroutine.cs
+++ src/Suspension/Coroutine.cs
@@ -5,10 +5,5 @@
         public abstract bool Completed { get; }
         public abstract T Result { get; }
         public abstract Coroutine<T> Run();
-
-        public abstract class Continuation<Argument>
-        {
-            public abstract Coroutine<T> Run(Argument argument);
-        }
     }
 }
