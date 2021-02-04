24bb0b7aa5b78cb65fe645848a9bfd0dc4c98f13
pepelev <kran440@gmail.com>
2021-02-04 20:54:16 +0500

    introduces PrettyGraph

--- src/Suspension.Tests/Playground.cs
+++ src/Suspension.Tests/Playground.cs
@@ -4,8 +4,11 @@ using System.Linq;
 using System.Text;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.FlowAnalysis;
 using NUnit.Framework;
 using Suspension.SourceGenerator.Generator;
+using Suspension.Tests.Samples;
 using static Suspension.Tests.Samples.While.Coroutines;
 
 namespace Suspension.Tests
@@ -16,7 +19,7 @@ namespace Suspension.Tests
         [Test]
         public void Test()
         {
-            var code = File.ReadAllText("Samples/Cycles/While.cs", Encoding.UTF8);
+            var code = File.ReadAllText("Samples/While.cs", Encoding.UTF8);
             var tree = CSharpSyntaxTree.ParseText(code);
             var compilation = CSharpCompilation.Create(
                 "Suspension.Tests.Samples",
@@ -48,6 +51,54 @@ namespace Suspension.Tests
             {
                 execute = execute.Run();
             }
+        }
+
+        [Test]
+        [TestCase(nameof(Throw.SimpleThrow))]
+        [TestCase(nameof(Throw.ThrowFromIf))]
+        [TestCase(nameof(Throw.TryCatch))]
+        [TestCase(nameof(Throw.TryCatchWhen))]
+        [TestCase(nameof(Throw.TryTwoCatch))]
+        [TestCase(nameof(Throw.TryTwoCatchWhen))]
+        [TestCase(nameof(Throw.Using))]
+        [TestCase(nameof(Throw.UsingNull))]
+        [TestCase(nameof(Throw.TryCatchFinally))]
+        public void Pretty(string methodName)
+        {
+            var code = File.ReadAllText("Samples/Throw.cs", Encoding.UTF8);
+            var tree = CSharpSyntaxTree.ParseText(code);
+            var compilation = CSharpCompilation.Create(
+                "Suspension.Tests.Samples",
+                new[] { tree },
+                new[]
+                {
+                    MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                    MetadataReference.CreateFromFile(@"C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.1.11\mscorlib.dll"),
+                    MetadataReference.CreateFromFile(@"C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.1.11\System.dll"),
+                    MetadataReference.CreateFromFile(@"C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.1.11\System.Core.dll"),
+                    MetadataReference.CreateFromFile(@"C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.1.11\System.Runtime.dll"),
+                    MetadataReference.CreateFromFile(@"C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.1.11\System.Console.dll"),
+                    MetadataReference.CreateFromFile(@"C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.1.11\System.Xml.dll"),
+                    MetadataReference.CreateFromFile(typeof(Coroutine).Assembly.Location),
+                    MetadataReference.CreateFromFile("FluentAssertions.dll"),
+                    MetadataReference.CreateFromFile("nunit.framework.dll")
+                },
+                new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary)
+            );
+
+            var result = compilation.Emit(Stream.Null);
+            if (!result.Success)
+                throw new Exception("Compilation failed");
+
+            var semantic = compilation.GetSemanticModel(tree);
+            var syntax = tree.GetRoot()
+                .DescendantNodes()
+                .OfType<MethodDeclarationSyntax>()
+                .Single(method => semantic.GetDeclaredSymbol(method)?.Name == methodName);
+
+            var graph = ControlFlowGraph.Create(syntax, semantic);
+            var pretty = new PrettyGraph(graph);
+            Console.WriteLine(pretty);
         }
     }
 }
--- src/Suspension.Tests/Samples/Throw.cs
+++ src/Suspension.Tests/Samples/Throw.cs
@@ -1,30 +1,217 @@
 ﻿using System;
-using FluentAssertions;
-using NUnit.Framework;
+//using FluentAssertions;
+//using NUnit.Framework;
 
 namespace Suspension.Tests.Samples
 {
     public partial class Throw
     {
         [Suspendable]
-        public static void Execute()
+        public static void SimpleThrow()
         {
             throw new Exception("Boom");
         }
 
-        [Test]
-        public void Should_Throw_Exception()
+        public static void ThrowFromIf(int a)
         {
-            var entry = new Coroutines.Execute.Entry();
+            {
+                var b = 30;
+                if (a < 42)
+                    throw new Exception("inside if");
 
-            var exception = Assert.Throws<Exception>(
-                () => entry.Run()
-            );
-            exception.Message.Should().Be("Boom");
-            exception.StackTrace.Should().MatchRegex(
-                @"^   at Suspension.Tests.Samples.Throw.Coroutines.Execute.Entry.Run\(\)" +
-                @" in .*\\Suspension\\src\\Suspension.Tests\\Samples\\Throw.cs:line 12"
-            );
+                a += b;
+            }
+
+            Console.WriteLine(a);
         }
+
+        public static void TryCatch(int a)
+        {
+            try
+            {
+                Console.WriteLine(a);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public static void TryCatchFinally(int a)
+        {
+            try
+            {
+                Console.WriteLine(a);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                Console.WriteLine("Finally");
+            }
+        }
+
+        public static void TryTwoCatch(int a)
+        {
+            try
+            {
+                Console.WriteLine(a);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public static void TryCatchWhen(int a)
+        {
+            try
+            {
+                Console.WriteLine(a);
+            }
+            catch (Exception e) when (e.Message[0] > 48)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private sealed class A : IDisposable
+        {
+            public void Dispose()
+            {
+            }
+        }
+
+        public static void Using(int a)
+        {
+            using (new A())
+            {
+                Console.WriteLine(a);
+            }
+        }
+
+        public static void UsingNull(int a)
+        {
+            using (null)
+            {
+                Console.WriteLine(a);
+            }
+        }
+
+        public static void TwoTry(int a)
+        {
+            try
+            {
+                var p = "asdad".GetHashCode();
+                try
+                {
+                    Console.WriteLine(a);
+                    if (a < 40)
+                    {
+                        Flow.Suspend("ALess40");
+                    }
+
+                    Console.WriteLine(a + 7);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public static void TryTwoCatchWhen(int a)
+        {
+            try
+            {
+                Console.WriteLine(a);
+                if (a < 40)
+                {
+                    Flow.Suspend("ALess40");
+                }
+
+                Console.WriteLine(a + 7);
+            }
+            catch (ArgumentException e) when (e.Message[0] > 48)
+            {
+                Flow.Suspend("Argument");
+                Console.WriteLine(e);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public static void TryTwoCatchWhenEntry(int a)
+        {
+            try
+            {
+                Console.WriteLine(a);
+                if (a < 40)
+                {
+                    TryTwoCatchWhenALess40(a);
+                    return;
+                }
+
+                Console.WriteLine(a + 7);
+            }
+            catch (ArgumentException e) when (e.Message[0] > 48)
+            {
+                TryTwoCatchWhenArgument(e);
+                return;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public static void TryTwoCatchWhenALess40(int a)
+        {
+            try
+            {
+                Console.WriteLine(a + 7);
+            }
+            catch (ArgumentException e) when (e.Message[0] > 48)
+            {
+                Flow.Suspend("Argument");
+                Console.WriteLine(e);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public static void TryTwoCatchWhenArgument(Exception e)
+        {
+            Console.WriteLine(e);
+        }
+
+        //[Test]
+        //public void Should_Throw_Exception()
+        //{
+        //    var entry = new Coroutines.SimpleThrow.Entry();
+
+        //    var exception = Assert.Throws<Exception>(
+        //        () => entry.Run()
+        //    );
+        //    exception.Message.Should().Be("Boom");
+        //    exception.StackTrace.Should().MatchRegex(
+        //        @"^   at Suspension.Tests.Samples.Throw.Coroutines.Execute.Entry.Run\(\)" +
+        //        @" in .*\\Suspension\\src\\Suspension.Tests\\Samples\\Throw.cs:line 12"
+        //    );
+        //}
     }
 }
